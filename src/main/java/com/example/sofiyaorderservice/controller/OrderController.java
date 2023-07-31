0fcd6591e1879c777d04fc8b4daa1d8282929b39
Yusupov Ulugbek <yusupovulugbek73@gmail.com>
2023-07-31 15:11:07 +0500

    order

--- src/main/java/com/example/sofiyaorderservice/controller/OrderController.java
+++ src/main/java/com/example/sofiyaorderservice/controller/OrderController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/order")
+@RequestMapping("order/api/v1")
 @RequiredArgsConstructor
 public class OrderController {
 
